fd3c0a1ea46dc1707691421808f51a07dd23b964
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-27 09:36:32 -0300

    cap 2 vid 2 finalizado

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require("apollo-server");
 const { mergeTypeDefs } = require("graphql-tools");
 const path = require('path')
 
+const matriculaSchema = require('./matricula/schema/matricula.graphql')
 const userSchema = require("./user/schema/user.graphql");
 const userResolvers = require("./user/resolvers/userResolvers");
 const UsersAPI = require("./user/datasource/user");
