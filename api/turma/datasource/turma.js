ed1d9b24c0fcb287816bf270a42c8da3317ce127
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-27 08:50:02 -0300

    cap 1 finalizado

--- api/turma/datasource/turma.js
+++ api/turma/datasource/turma.js
@@ -3,12 +3,54 @@ const { SQLDataSource } = require("datasource-sql")
 class TurmasAPI extends SQLDataSource {
   constructor(dbConfig) {
     super(dbConfig)
+    this.Resposta = {
+      mensagem: ""
+    }
   }
 
   async getTurmas() {
-    return this.db
+    return await this.db
       .select('*')
       .from('turmas')
+  }
+
+  async getTurma(id) {
+    const turma = await this.db
+      .select('*')
+      .from('turmas')
+      .where({id: Number(id)})
+    return turma[0]
+  }
+
+  async incluiTurma(turma) {
+    const novaTurma = await this.db
+      .insert({...turma})
+      .returning(turma.id)
+      .into('turmas')
+    const turmaInserida = await this.getTurma(novaTurma[0])
+    return ({...turmaInserida})
+  }
+
+  async atualizaTurma(dados) {
+    await this.db.update({
+      ...dados.turma
+    })
+    .where({id: Number(dados.id)})
+    .into('turmas')
+    const turmaAtualizada = await this.getTurma(dados.id)
+    return ({
+      ...turmaAtualizada
+    })
+  }
+
+  async deletaTurma(id) {
+    await this.db('turmas')
+      .where({id: Number(id)})
+      .del()
+    
+    this.Resposta.mensagem = `registro de id ${id} deletado com sucesso`
+
+    return this.Resposta
   }
 
 }
--- api/turma/resolvers/turmaResolvers.js
+++ api/turma/resolvers/turmaResolvers.js
@@ -1,7 +1,23 @@
+const { GraphQLScalarType } = require("graphql");
 const turmaResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "string de data e hora no formato ISO-8601",
+    serialize: (value) => new Date(value).toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value).toISOString(),
+  }),
   Query: {
-    turmas: (root, args, {dataSources}, info) => dataSources.turmasAPI.getTurmas() 
+    turmas: (root, args, { dataSources }, info) =>
+      dataSources.turmasAPI.getTurmas(),
+    turma: (root, { id }, { dataSources }, info) =>
+      dataSources.turmasAPI.getTurma(id),
+  },
+  Mutation: {
+    incluiTurma: (root, {turma}, {dataSources}) => dataSources.turmasAPI.incluiTurma(turma),
+    atualizaTurma: (root, dados, {dataSources}) => dataSources.turmasAPI.atualizaTurma(dados),
+    deletaTurma: (root, {id}, {dataSources}) => dataSources.turmasAPI.deletaTurma(id)
   }
-}
+};
 
-module.exports = turmaResolvers
+module.exports = turmaResolvers;
